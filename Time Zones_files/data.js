19c84853e9f68f2c78b7bcdb77e6bded8cf17b96
w0ts0n <w0ts0n@gmail.com>
2019-05-23 15:15:27 -0500

    bravetz from everytimezone
    bravetz from everytimezone, ripped out analytics and other extra js.

---
+++ Time Zones_files/data.js
@@ -0,0 +1,1 @@
+var data = [[-0.0,"UTC","UTC-0",true],[-7.0,"San Fransico","UTC-7",true],[-4.0,"Ontario","UTC-7",true],[+1.0,"London","UTC+1",true],[-5.0,"Grand Cayman","UTC-5",true],[9.0,"Tokyo","UTC+9",true]];
